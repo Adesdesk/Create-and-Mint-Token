4d0a1f010d19cf6ddc4f9681adc9d6b1e7f3bb8f
Adeola David Adelakun <aadelakun28@gmail.com>
2023-06-08 12:30:12 +0100

    deployed version

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,9 +2,18 @@ require("@nomicfoundation/hardhat-toolbox");
 require("hardhat-deploy");
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
 module.exports = {
-  solidity: '0.8.9',
+  solidity: "0.8.7",
   networks: {
-    hardhat: {},
+    hardhat: {
+      chainId: 1337, // Chain ID of the Hardhat local blockchain
+    },
+  },
+  paths: {
+    artifacts: "./artifacts",
+    cache: "./cache",
+    sources: "./contracts",
+    tests: "./test",
   },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,20 +1,36 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+
+// Deploy script for CustomToken smart contract on Hardhat
+
+// Import Hardhat environment and Ethereum libraries
 const { ethers } = require("hardhat");
 
 async function main() {
+  // Set up Ethereum wallet
   const [deployer] = await ethers.getSigners();
 
-  console.log("Deploying contracts with the account:", deployer.address);
-
+  // Grab MyCustomToken.sol
+  console.log("Deploying the CustomToken contract with the account:", deployer.address);
+  // Set up the CustomToken contract factory
   const CustomToken = await ethers.getContractFactory("CustomToken");
-  const supply = ethers.utils.parseEther('100000');
-  const customToken = await CustomToken.deploy("MyCustomToken", "MCT", supply);
+  const initialSupply = ethers.utils.parseEther("1000");
+  // Deploy the CustomToken contract
+  const customToken = await CustomToken.deploy("MyCustomToken", "MCT", initialSupply);
 
-  console.log("CustomToken deployed to:", customToken.address);
+  await customToken.deployed();
+  // display success and address
+  console.log("CustomToken contract deployed to address:", customToken.address);
+  
 }
 
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+
+// Run the main function
+main().then(() => process.exit(0)).catch(error => {
+  console.error(error);
+  process.exit(1);
+});
